24184a3b5defe3edf650cc98e133a67908bd3db4
xenoteo <fedorovxa@gmail.com>
2020-12-15 18:18:28 +0100

    Unidecode added, database filled, more request handlers added

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import dbmanage as db
+import string
+from unidecode import unidecode
 
 
 class Bot:
     def __init__(self):
-        self.goodbye = ["do zobaczenia", "do widzenia"]
+        self.GOODBYE = ["do zobaczenia", "do widzenia"]
+        self.HELP = ["help", "pomocy", "pomoz"]
+        self.SCHEDULE = ["grafik", "harmonogram", "kalendarz"]
         self.chatbot = ChatBot('Harold Fit', database_uri='sqlite:///fit.db')
         self.train()
         self.db_manager = db.DBManager()
@@ -15,23 +19,70 @@ class Bot:
         trainer.train("corpus.classes")
         trainer.train("corpus.greetings")
         trainer.train("corpus.goodbye")
+        trainer.train("corpus.thank-you")
+
+    def is_goodbye(self, request):
+        if request in self.GOODBYE:
+            return True
+        for goodbye in self.GOODBYE:
+            if request.__contains__(goodbye):
+                return True
+        return False
+
+    def is_help(self, request):
+        if request in self.HELP:
+            return True
+        for word in self.HELP:
+            if request.__contains__(word):
+                return True
+        return False
+
+    def is_schedule(self, request):
+        if request in self.SCHEDULE:
+            return True
+        for word in self.SCHEDULE:
+            if request.__contains__(word):
+                return True
+        return False
+
+    def print_help(self):
+        print("Nazywam się Harold Fit i mam przyjemność pomagać klientom z harmonogramem zajęć fitnesowych :)\n"
+              "Między innymi mogę znaleźć zajęcia wybranego instruktora, w wybranym dniu, wybranego typu "
+              "albo dopasować trening do Twoich wymagań ;)")
 
     def run(self):
+        print("\nWitam! Jestem botem i nazywam się Harold Fit. Mam przyjemność pomagać klientom "
+              "z harmonogramem zajęć fitnesowych :)\n"
+              "Między innymi mogę znaleźć zajęcia wybranego instruktora, w wybranym dniu, wybranego typu "
+              "albo dopasować trening do Twoich wymagań ;)\n"
+              "Serdecznie zapraszam do dialogu!")
         running = True
         while running:
-            request = input("Powiedz coś: ")
-            request_lower = request.lower()
-            if request_lower in self.goodbye:
+            request = unidecode(input("Powiedz coś: ").lower().translate(str.maketrans('', '', string.punctuation)))
+
+            if self.is_goodbye(request):
                 running = False
 
             response = True
-            if self.db_manager.check_days(request_lower):
+
+            if self.is_help(request):
+                self.print_help()
                 response = False
 
-            if self.db_manager.check_classes(request_lower):
+            if self.is_schedule(request):
+                self.db_manager.print_schedule()
                 response = False
 
-            if self.db_manager.check_instructors(request_lower):
+            if response and self.db_manager.check_days(request):
+                response = False
+
+            if response and self.db_manager.check_classes(request):
+                response = False
+
+            if response and self.db_manager.check_instructors(request):
+                response = False
+
+            if response and self.db_manager.check_descriptions(request):
                 response = False
 
             if response:
--- dbmanage.py
+++ dbmanage.py
@@ -1,32 +1,59 @@
 import sqlite3
+from unidecode import unidecode
 
 
 class DBManager:
     def __init__(self):
         self.conn = sqlite3.connect('fit.db')
         self.c = self.conn.cursor()
-        self.CLASSES = self.get_all_classes()
-        self.INSTRUCTORS = self.get_all_instructors()
-        self.DAYS_OF_WEEK = ["poniedziałek"]
+        self.CLASSES_MAP = dict()      # unidecoded -> ununidecoded
+        self.CLASSES_UNIDECODED = []
+        self.INSTRUCTORS_MAP = {}   # unidecoded -> ununidecoded
+        self.INSTRUCTORS_UNIDECODED = []
+        self.DAYS_OF_WEEK_MAP = dict()  # unidecoded -> ununidecoded
+        self.DAYS_OF_WEEK_UNIDECODED = []
+        self.DESCRIPTIONS_MAP = {}  # unidecoded -> ununidecoded
+        self.DESCRIPTIONS_UNIDECODED = []
 
-    def get_all_classes(self):
+        self.set_all_classes()
+        self.set_all_instructors()
+        self.set_days_of_week()
+        self.set_all_descriptions()
+
+    def set_all_classes(self):
         self.c.execute('SELECT name FROM classes')
-        classes = []
         for fit in self.c.fetchall():
-            classes.append(fit[0])
-        return classes
+            unidecoded = unidecode(fit[0])
+            self.CLASSES_UNIDECODED.append(unidecoded)
+            self.CLASSES_MAP[unidecoded] = fit[0]
 
-    def get_all_instructors(self):
+    def set_all_instructors(self):
         self.c.execute('SELECT DISTINCT instructor FROM classes')
-        instructors = []
         for fit in self.c.fetchall():
-            instructors.append(fit[0].lower())
-        return instructors
+            instructor = fit[0].lower()
+            instructor_unidecoded = unidecode(instructor)
+            self.INSTRUCTORS_UNIDECODED.append(instructor_unidecoded)
+            self.INSTRUCTORS_MAP[instructor_unidecoded] = instructor
+
+    def set_days_of_week(self):
+        days_of_week = ["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"]
+        for day in days_of_week:
+            unidecoded = unidecode(day)
+            self.DAYS_OF_WEEK_UNIDECODED.append(unidecoded)
+            self.DAYS_OF_WEEK_MAP[unidecoded] = day
+
+    def set_all_descriptions(self):
+        self.c.execute('SELECT DISTINCT description FROM classes')
+        for fit in self.c.fetchall():
+            description = fit[0].lower()
+            unidecoded = unidecode(description)
+            self.DESCRIPTIONS_UNIDECODED.append(unidecoded)
+            self.DESCRIPTIONS_MAP[unidecoded] = description
 
     def check_days(self, request):
-        for day in self.DAYS_OF_WEEK:
+        for day in self.DAYS_OF_WEEK_UNIDECODED:
             if request.__contains__(day):
-                self.c.execute('SELECT * FROM classes WHERE day=?', (day,))
+                self.c.execute('SELECT * FROM classes WHERE day=?', (self.DAYS_OF_WEEK_MAP[day],))
                 output = ""
                 for fit in self.c.fetchall():
                     output += f"{fit[2]}:00-{fit[2]}:50 {fit[0]}\n"
@@ -35,23 +62,51 @@ class DBManager:
         return False
 
     def check_classes(self, request):
-        for training in self.CLASSES:
+        for training in self.CLASSES_UNIDECODED:
             if request.__contains__(training):
-                self.c.execute('SELECT description FROM classes WHERE name=?', (training,))
-                print(self.c.fetchone()[0])
+                self.c.execute('SELECT description FROM classes WHERE name=?', (self.CLASSES_MAP[training],))
+                output = f"{training} to {self.c.fetchone()[0]}. Zajęcia odbywają się:\n"
+                self.c.execute('SELECT * FROM classes WHERE name=?', (self.CLASSES_MAP[training],))
+                for fit in self.c.fetchall():
+                    output += f"{fit[2]}:00-{fit[2]}:50 {fit[1]}\n"
+                print(output, end="")
                 return True
         return False
 
     def check_instructors(self, request):
-        for instructor in self.INSTRUCTORS:
+        for instructor in self.INSTRUCTORS_UNIDECODED:
             if request.__contains__(instructor):
-                self.c.execute('SELECT * FROM classes WHERE instructor=?', (instructor,))
+                self.c.execute('SELECT * FROM classes WHERE instructor=?', (self.INSTRUCTORS_MAP[instructor],))
                 output = ""
                 for fit in self.c.fetchall():
-                    output += f"{fit[2]}:00-{fit[2]}:50 {fit[1]} {fit[0]}\n"
+                    output += f"{fit[2]}:00-{fit[2]}:50 {fit[1].ljust(15)} {fit[0].ljust(20)}\n"
                 print(output, end="")
                 return True
         return False
+
+    def print_trainings_with_description(self, description):
+        self.c.execute('SELECT * FROM classes WHERE description=?', (description,))
+        output = ""
+        for fit in self.c.fetchall():
+            output += f"{fit[2]}:00-{fit[2]}:50 {fit[1].ljust(15)} {fit[0].ljust(20)} {fit[4]}\n"
+        print(output, end="")
+
+    def check_descriptions(self, request):
+        founded = False
+        for word in request.split():
+            if word != 'trening' and word != 'zajecia' and len(word) > 2:
+                for description in self.DESCRIPTIONS_UNIDECODED:
+                    if description.__contains__(word):
+                        self.print_trainings_with_description(self.DESCRIPTIONS_MAP[description])
+                        founded = True
+        return founded
+
+    def print_schedule(self):
+        self.c.execute('SELECT * FROM classes ORDER BY id')
+        output = ""
+        for fit in self.c.fetchall():
+            output += f"{fit[2]}:00-{fit[2]}:50 {fit[1].ljust(15)} {fit[0].ljust(20)} {fit[3].ljust(20)} {fit[4]}\n"
+        print(output, end="")
 
     def close_connection(self):
         self.conn.close()
